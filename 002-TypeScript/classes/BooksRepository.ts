1d3e8c626099cf799ce52eedc7560ba4ea4112b7
“Antena75” <a.korotaev75@gmail.com>
2025-10-28 17:30:00 +0500

    Задание 002-Typescript исправленное

--- 002-TypeScript/classes/BooksRepository.ts
+++ 002-TypeScript/classes/BooksRepository.ts
@@ -1,43 +1,21 @@
-const {books} = require('../library/tbooks');
+// const {books} = require('../library/tbooks');
 
-abstract class BooksRepository{
-    createBook(book: IBook):void {        
-        const idx = books.findIndex( (el:IBook) => el.id === book.id);
-        if (idx !== -1) return books[idx];
-        books.push(book);
+abstract class BooksRepository{    
+    constructor(public books: IBook[]) {}
+    
+    getBooks(): IBook[] {
+        return this.books;
     }
-    getBook(id: string):IBook | null {
-        const idx = books.findIndex( (el:IBook) => el.id === id);
+
+    getBook(id: string): IBook | null {
+        const idx = this.books.findIndex( (el:IBook) => el.id === id);
         if (idx === -1) return null;        
-        return books[idx];        
+        return this.books[idx];        
     }
-    getBooks():IBook[] {
-        return books;
-    }
-    updateBook(
-        id: string, 
-        title: string, 
-        description: string, 
-        authors: string, 
-        favorite: boolean, 
-        fileCover: string, 
-        fileName: string, 
-        fileBook: string):void {
-            
-        const idx = books.findIndex( (el:IBook) => el.id === id);
-        if (idx === -1) return;   
-        books[idx].title = title;     
-        books[idx].description = description;   
-        books[idx].authors = authors;    
-        books[idx].favorite = favorite;     
-        books[idx].fileCover = fileCover;     
-        books[idx].fileName = fileName;     
-        books[idx].fileBook = fileBook;     
-    }
-    deleteBook(id: string): void {
-        const idx = books.findIndex( (el:IBook) => el.id === id);
-        if (idx === -1) return; 
-        let book = books[idx];       
-        books.splice(idx, 1);
-    }
+
+    abstract createBook(book: IBook): void;
+
+    abstract updateBook(id: number, updatedBook: IBook): void;
+
+    abstract deleteBook(id: string): void;
 }
